59beb6e251eb2e3ad5aeee18026bbcced32e4463
tddawson <tddawson@gmail.com>
2014-03-27 14:30:30 -0600

    Changed settings.py to config for MySQL. You'll have to create a MySQL database with the name, port, and credentials indicated.

--- app/settings.py
+++ app/settings.py
@@ -57,8 +57,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'project',
+	'USER': 'root',
+	'PASSWORD': 'root',
+	'HOST':'localhost',
+	'PORT':'3306'
+	
     }
 }
 
