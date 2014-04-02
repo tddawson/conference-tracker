9da8e8a427b9b57d83c02934d2b79c6a6a04f5be
tddawson <tddawson@gmail.com>
2014-04-02 16:34:48 -0600

    Added the explore templates with some fake static data.  You can click on any of the links under 'Browse General Conference' to access them.

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,4 +5,6 @@ from tracker import views
 urlpatterns = patterns('',
 	url(r'^$', views.home, name='home'),
     url(r'^home/', views.home, name='home'),
+    url(r'^explore(?P<sort_by>)/$', views.explore, name='explore'),
+    url(r'^explore/(?P<sort_by>\w+)/', views.explore, name='explore'),
 )
--- tracker/views.py
+++ tracker/views.py
@@ -4,3 +4,15 @@ def home(request):
 	sample_list = ["one", "two", "three"]
 	context = {'var_name': sample_list}
 	return render(request, 'tracker/home.html', context)
+
+def explore(request, sort_by):
+	if sort_by == "":
+		sort_by = "sessions"
+
+	data = {}
+	data['sessions'] = ["October 2013", "April 2013", "October 2012", "April 2012"]
+	data['topics'] = ["Faith", "Repentance", "Baptism", "Holy Ghost"]
+	data['speakers'] = ["President Thomas S. Monson", "President Dieter F. Uchtdorf", "President Henry B. Eyring"]
+
+	context = {'categories': data[sort_by], "sort_by": sort_by[:-1]}
+	return render(request, 'tracker/explore.html', context)
