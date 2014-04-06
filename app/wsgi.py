eeeaae8a4fddfe41523803e8bd1ad3f3d8f48546
administrator <admin@GospelGoodReads.(none)>
2014-04-06 18:25:48 -0400

    Changed the wsgi config on the server to set the correct class path. Now at least serves up views (just not templates or static files like CSS, images)

--- app/wsgi.py
+++ app/wsgi.py
@@ -6,8 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
+import sys
 import os
+
+sys.path.insert(0, '/home/admin/public_html/conference-tracker/conference-tracker/')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 from django.core.wsgi import get_wsgi_application
