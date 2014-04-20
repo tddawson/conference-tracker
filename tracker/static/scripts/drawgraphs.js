7f52b7904f11d8ef574f58e413c57e05dae9f98f
tddawson <tddawson@gmail.com>
2014-04-19 22:04:54 -0600

    Switched from pie charts back to progress bars at bottom because I like it better.

--- tracker/static/scripts/drawgraphs.js
+++ tracker/static/scripts/drawgraphs.js
@@ -1,17 +1,34 @@
 $(document).ready(function() {
 	var ratios = $('.completion-ratio');
 	$.each(ratios, function( index, div ) {
-		var s = $(div).find('span').html().split("/");
-		var canvas = document.getElementsByTagName('canvas')[index];
-
-		var complete = parseInt(s[0]);
-		var incomplete = parseInt(s[1]) - complete;
-		
-		drawPieChart(canvas, complete, incomplete);
+		progressBar(index, div);
+		//pieChart(index,div);
 	});
 
 });
 
+
+function progressBar(index, div) {
+	var s = $(div).html().split("/");	
+	var complete = parseInt(s[0]);
+	var total = parseInt(s[1]);
+	var percent = parseInt(100 * complete / total);
+
+	var bar = $('.progress-tracker:eq(' + index + ')');//.get(index);
+	bar.width(percent + "%");
+	bar.attr('data-content', percent + "%");
+}
+
+function pieChart(index, div) {
+	var s = $(div).find('span').html().split("/");
+	var canvas = document.getElementsByTagName('canvas')[index];
+
+	var complete = parseInt(s[0]);
+	var incomplete = parseInt(s[1]) - complete;
+		
+	drawPieChart(canvas, complete, incomplete);
+
+}
 
 function drawPieChart(canvas, complete, incomplete)
 {
